3f0e7e811876ea2362778a355b8f6bb8735e0fab
code5TAJL <asmir.sulejmanovic@outlook.com>
2022-04-12 00:18:55 +0200

    Day15 finished

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,4 +1,7 @@
 // Only change code below this line
-const sum = (num1 = 1, num2) => 
+const sum = (num1, num2 = 1) => num1 + num2 
+
+console.log(sum(4, 6));
+console.log(sum(3));
 // Only change code above this line
 module.exports = sum;
--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,8 +1,11 @@
 // Only change code below this line
-const product = (aa, bb, cc) => {
-    const args = [aa, bb, cc];
+const product = (...args) => {
+    
 // Only change code above this line
     return args.reduce((a, b) => a * b, 1);
 }
-console.log(product()); // Change this line
+console.log(product(1, 2)); // Change this line
+console.log(product(4, 3, 10, 2));
+console.log(product(7));
+console.log(product(1));
 module.exports = product;
